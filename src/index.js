a13f7f3577057bd897b3c4e838f627006f6c6cb8
Timur Davlet <timazavr@mail.ru>
2021-04-13 00:51:16 +0300

    подключение линтира

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const start = () => {
   console.log('Привет! Я эмоциональный бот!');
   const name = readlineSync.question('Как тебя зовут? ');
   const mood = readlineSync.question(`${name}, какое у тебя сейчас настроенеие?`);
-}
+};
 
 export default start;
